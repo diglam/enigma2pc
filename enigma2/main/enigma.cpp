b0d8226926707f4b156ecb5df582a0a05fbb0081
neo <neo@yavdr-0.6>
2015-07-11 11:15:58 +0200

    eVideoWidget: optimize, clean up global stuff, and use apply
    From Commit [8e1e66] Authored by: pieterg 2011-11-09

--- enigma2/main/enigma.cpp
+++ enigma2/main/enigma.cpp
@@ -233,8 +233,7 @@ int main(int argc, char **argv)
 	python.execFile(eEnv::resolve("${libdir}/enigma2/python/mytest.py").c_str());
 
 	/* restore both decoders to full size */
-	extern void setFullsize();
-	setFullsize();
+	eVideoWidget::setFullsize();
 
 	if (exit_code == 5) /* python crash */
 	{
