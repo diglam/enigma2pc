ca206061702209790477e5a3d563683cf497d511
neo <neo@yavdr-0.6>
2015-07-12 19:50:13 +0200

    Directly fallback to current service when previous PiP service is not possible
    From Commit [920887] Authored by: Littlesat 2014-06-07

--- enigma2/lib/python/Screens/InfoBarGenerics.py
+++ enigma2/lib/python/Screens/InfoBarGenerics.py
@@ -1965,9 +1965,14 @@ class InfoBarPiP:
 				self.session.pip.servicePath = self.servicelist.getCurrentServicePath()
 				self.reclocatePiP()
 			else:
-				self.lastPiPService = None
-				self.session.pipshown = False
-				del self.session.pip
+				newservice = self.session.nav.getCurrentlyPlayingServiceReference() or self.servicelist.servicelist.getCurrent()
+				if self.session.pip.playService(newservice):
+					self.session.pipshown = True
+					self.session.pip.servicePath = self.servicelist.getCurrentServicePath()
+				else:
+					self.lastPiPService = None
+					self.session.pipshown = False
+					del self.session.pip
 
 	def activePiP(self):
 		if self.session.pipshown:
