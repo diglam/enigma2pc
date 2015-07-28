f0c3bba191e6388d2df243b70aa6011d89112f40
neo <neo@yavdr-0.6>
2015-07-28 17:50:54 +0200

    HelpMenuList: increase the length of the text
    As suggested by dima73
    
    From Commit [7aaca8] Authored by: littlesat 2014-06-25

--- enigma2/lib/python/Components/HelpMenuList.py
+++ enigma2/lib/python/Components/HelpMenuList.py
@@ -39,19 +39,19 @@ class HelpMenuList(GUIComponent):
 
 				if flags & 8: # for long keypresses, prepend l_ into the key name.
 					name = (name[0], "long")
-					
+
 				entry = [ (actionmap, context, action, name ) ]
 
 				if isinstance(help, list):
 					self.extendedHelp = True
 					print "extendedHelpEntry found"
 					entry.extend((
-						(eListboxPythonMultiContent.TYPE_TEXT, 0, 0, 500, 26, 0, 0, help[0]),
-						(eListboxPythonMultiContent.TYPE_TEXT, 0, 28, 500, 20, 1, 0, help[1])
+						(eListboxPythonMultiContent.TYPE_TEXT, 0, 0, 600, 26, 0, 0, help[0]),
+						(eListboxPythonMultiContent.TYPE_TEXT, 0, 28, 600, 20, 1, 0, help[1])
 					))
 				else:
-					entry.append( (eListboxPythonMultiContent.TYPE_TEXT, 0, 0, 500, 28, 0, 0, help) )
-					
+					entry.append( (eListboxPythonMultiContent.TYPE_TEXT, 0, 0, 600, 28, 0, 0, help) )
+
 				l.append(entry)
 
 		self.l.setList(l)
